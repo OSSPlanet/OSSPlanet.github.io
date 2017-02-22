f536ff6c73fe2eebdc945cd3864adbebe0a1bb20
YY <z3388638@gmail.com>
2017-02-23 02:32:16 +0800

    loop highlights

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,44 @@ $('a[href*=#]:not([href=#])').click(function() {
       }
     }
 });
+
+/* loop highlights */
+var $highlights = $('.highlights');
+var $media = $highlights.find('.media');
+var num = $media.size();
+var order = [];
+for(let i = 0; i < num; i++) {
+  order = [...order, i];
+}
+order = shuffle(order);
+loopHighlights(0)
+
+function loopHighlights(index) {
+  if(order[index] == undefined) {
+      index = 0;
+    }
+  $media.hide()
+    $media.eq(order[index]).fadeIn(500)
+    setTimeout(function() {
+      loopHighlights(index + 1);
+    }, 5000)
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
